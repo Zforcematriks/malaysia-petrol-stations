d86b6c2b3698f12968278e0eec22222c322532a2
Zforcematriks <jinnhonn@gmail.com>
2025-06-23 21:35:13 +0800

    fix: 正确设置 use client 和 dynamic，避免预渲染错误

--- src/app/stations/page.tsx
+++ src/app/stations/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+export const dynamic = "force-dynamic";
 import Image from "next/image";
 import React, { useState, useEffect, useMemo } from "react";
 import { useSearchParams } from "next/navigation";
